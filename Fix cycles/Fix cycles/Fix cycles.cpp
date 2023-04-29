f6ce2c187de60b2ba84fe6badc40e836d7347436
xFogS <131852082+xFogS@users.noreply.github.com>
2023-04-29 15:11:45 +0300

    Add files via upload

---
+++ Fix cycles/Fix cycles/Fix cycles.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	//For 15. Дано вещественное число A и целое число N(> 0).Найти A в степени N :
+//A^N = A·A· . ..·A (числа A перемножаются N раз).
+	//double A = 1.4;
+	//int N = 5;
+	//int S = 1;
+	//for (int i = 0; i < N; i++)
+	//{
+	//	A = N;
+	//	S *= A;
+	//}
+	//cout << S << endl;
+}
